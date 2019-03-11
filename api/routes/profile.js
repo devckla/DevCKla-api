0780a5ed4f3ea6d4ab788a583d3ba87aa1ed3274
KengoWada <louiskengo3@gmail.com>
2019-03-11 08:29:37 +0300

    Added delete route

--- api/routes/profile.js
+++ api/routes/profile.js
@@ -4,31 +4,31 @@ const passport = require("passport");
 
 const testing = require("../controllers/profile/testing");
 const createProfile = require("../controllers/profile/createProfile")
-  .createProfile;
+	.createProfile;
 const updateProfile = require("../controllers/profile/updateProfile")
-  .updateProfile;
+	.updateProfile;
 
-// @route  GET /api/profile/testing
+// @route  GET /profile/testing
 // @desc   Testing the routing
 // @access Public
 router.get("/testing", testing.testing);
 
-// @route  GET /api/profile/create
+// @route  GET /profile/create
 // @desc   Create a user profile
 // @access Protected
 router.post(
-  "/create",
-  passport.authenticate("jwt", { session: false }),
-  createProfile
+	"/create",
+	passport.authenticate("jwt", { session: false }),
+	createProfile
 );
 
-// @route  GET /api/profile/update
+// @route  GET /profile/update
 // @desc   Update a user profile
 // @access Protected
 router.patch(
-  "/update",
-  passport.authenticate("jwt", { session: false }),
-  updateProfile
+	"/update",
+	passport.authenticate("jwt", { session: false }),
+	updateProfile
 );
 
 module.exports = router;
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,35 +1,50 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User");
+
 const testing = require("../controllers/testing").testing;
 const register = require("../controllers/users/register").register;
 const login = require("../controllers/users/login").loging;
 const forgot = require("../controllers/users/forgot").forgot;
 const reset = require("../controllers/users/reset").reset;
 
-// @route  GET /api/auth/testing
+// @route  GET /auth/testing
 // @desc   Testing the routing
 // @access Public
 router.get("/testing", testing);
 
-// @route  POST /api/auth/register
+// @route  POST /auth/register
 // @desc   Allow someone to register
 // @access Public
 router.post("/register", register);
 
-// @route  POST /api/auth/login
+// @route  POST /auth/login
 // @desc   Allow a user to login
 // @access Public
 router.post("/login", login);
 
-// @route  POST /api/auth/forgot
+// @route  POST /auth/forgot
 // @desc   Allow a user to reset their password
 // @access Public
 router.get("/forgot", forgot);
 
-// @route  POST /api/auth/login
+// @route  POST /auth/login
 // @desc   Allow a user to login
 // @access Protected
 router.get("/reset/:token", reset);
+
+// @route  DELETE /auth/delete
+// @desc   Delete all users created during testing
+// @access Protected
+router.delete("/delete", (req, res) => {
+	User.remove({})
+		.then(() => {
+			console.log("Done");
+		})
+		.catch(err => {
+			console.log(err);
+		});
+});
 
 module.exports = router;
